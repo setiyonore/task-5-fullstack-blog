dbae647151cbca079d2111f6144de9ec256ef081
setiyonore <setiyono.ressly@gmail.com>
2022-08-22 11:53:03 +0700

    add CategoryTest

---
+++ tests/Feature/CategoryTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use App\Models\Category;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CategoryTest extends TestCase
+{
+    use DatabaseMigrations;
+    use WithFaker;
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+
+    /**
+     * @test
+     */
+
+    public function user_can_see_index_category(){
+        $user = User::factory()->create();
+        $category = Category::factory()->create();
+        $this->actingAs($user)
+            ->get('/category')
+            ->assertStatus(200)
+            ->assertSee($category->name)
+            ->assertViewIs('pages.category.index');
+    }
+
+    /**
+     * @test
+     */
+    public function user_can_create_category() {
+        $user = User::factory()->create();
+        $this->actingAs($user)
+            ->get('/category/create')
+            ->assertStatus(200)
+            ->assertViewIs('pages.category.create');
+    }
+}
